f1221282f208687aad71af96296937955c2c72ba
Kilian Vounckx <kilianvounckx@hotmail.be>
2025-01-31 15:30:24 +0100

    :sparkles: Add parse_a

--- lib/carser.ml
+++ lib/carser.ml
@@ -1,4 +1,29 @@
-let%expect_test _ =
-  print_endline "Hello, World!";
-  [%expect {| Hello, World! |}]
+let parse_a input =
+  let length = String.length input in
+  if length == 0
+  then false, ""
+  else if String.get input 0 == 'a'
+  then true, String.sub input 1 (length - 1)
+  else false, input
+;;
+
+let%expect_test "abc" =
+  let input = "abc" in
+  let b, s = parse_a input in
+  Printf.printf "(%b, %s)" b s;
+  [%expect {| (true, bc) |}]
+;;
+
+let%expect_test "zbc" =
+  let input = "zbc" in
+  let b, s = parse_a input in
+  Printf.printf "(%b, %s)" b s;
+  [%expect {| (false, zbc) |}]
+;;
+
+let%expect_test "" =
+  let input = "" in
+  let b, s = parse_a input in
+  Printf.printf "(%b, %s)" b s;
+  [%expect {| (false, ) |}]
 ;;
