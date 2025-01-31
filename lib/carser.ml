cc24afd0f03048002d9febb88833f9bf5c350007
Kilian Vounckx <kilianvounckx@hotmail.be>
2025-01-31 15:34:49 +0100

    :sparkles: Add specified character parser

--- lib/carser.ml
+++ lib/carser.ml
@@ -1,29 +1,32 @@
-let parse_a input =
+let pchar expected input =
   let length = String.length input in
   if length == 0
-  then false, ""
-  else if String.get input 0 == 'a'
-  then true, String.sub input 1 (length - 1)
-  else false, input
+  then "no more input", ""
+  else if String.get input 0 == expected
+  then Printf.sprintf "%c" (String.get input 0), String.sub input 1 (length - 1)
+  else Printf.sprintf "expected %c, got %c" expected (String.get input 0), input
 ;;
 
 let%expect_test "abc" =
   let input = "abc" in
+  let parse_a = pchar 'a' in
   let b, s = parse_a input in
-  Printf.printf "(%b, %s)" b s;
-  [%expect {| (true, bc) |}]
+  Printf.printf "(%s, %s)" b s;
+  [%expect {| (a, bc) |}]
 ;;
 
 let%expect_test "zbc" =
   let input = "zbc" in
+  let parse_a = pchar 'a' in
   let b, s = parse_a input in
-  Printf.printf "(%b, %s)" b s;
-  [%expect {| (false, zbc) |}]
+  Printf.printf "(%s, %s)" b s;
+  [%expect {| (expected a, got z, zbc) |}]
 ;;
 
 let%expect_test "" =
   let input = "" in
+  let parse_a = pchar 'a' in
   let b, s = parse_a input in
-  Printf.printf "(%b, %s)" b s;
-  [%expect {| (false, ) |}]
+  Printf.printf "(%s, %s)" b s;
+  [%expect {| (no more input, ) |}]
 ;;
