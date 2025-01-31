e3a3b93de83a0f081109e997e0daa34480826f99
Kilian Vounckx <kilianvounckx@hotmail.be>
2025-01-31 15:45:36 +0100

    :sparkles: Add result type

--- lib/carser.ml
+++ lib/carser.ml
@@ -1,32 +1,41 @@
+type 'a parse_result = ('a, string) Result.t
+
+let pp_parse_result pp_ok oc = function
+  | Ok (x, rest) -> Printf.fprintf oc "Ok(%a, \"%s\")" pp_ok x rest
+  | Error message -> Printf.fprintf oc "Error(\"%s\")" message
+;;
+
 let pchar expected input =
   let length = String.length input in
   if length == 0
-  then "no more input", ""
+  then Error "no more input"
   else if String.get input 0 == expected
-  then Printf.sprintf "%c" (String.get input 0), String.sub input 1 (length - 1)
-  else Printf.sprintf "expected %c, got %c" expected (String.get input 0), input
+  then Ok (String.get input 0, String.sub input 1 (length - 1))
+  else Error (Printf.sprintf "expected %c, got %c" expected (String.get input 0))
 ;;
+
+let pp_char oc c = Printf.fprintf oc "%c" c
 
 let%expect_test "abc" =
   let input = "abc" in
   let parse_a = pchar 'a' in
-  let b, s = parse_a input in
-  Printf.printf "(%s, %s)" b s;
-  [%expect {| (a, bc) |}]
+  let result = parse_a input in
+  Printf.printf "%a" (pp_parse_result pp_char) result;
+  [%expect {| Ok(a, "bc") |}]
 ;;
 
 let%expect_test "zbc" =
   let input = "zbc" in
   let parse_a = pchar 'a' in
-  let b, s = parse_a input in
-  Printf.printf "(%s, %s)" b s;
-  [%expect {| (expected a, got z, zbc) |}]
+  let result = parse_a input in
+  Printf.printf "%a" (pp_parse_result pp_char) result;
+  [%expect {| Error("expected a, got z") |}]
 ;;
 
 let%expect_test "" =
   let input = "" in
   let parse_a = pchar 'a' in
-  let b, s = parse_a input in
-  Printf.printf "(%s, %s)" b s;
-  [%expect {| (no more input, ) |}]
+  let result = parse_a input in
+  Printf.printf "%a" (pp_parse_result pp_char) result;
+  [%expect {| Error("no more input") |}]
 ;;
