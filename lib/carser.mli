86e9d06f4ec785179a6deba7fcd353d87106749a
Kilian Vounckx <kilianvounckx@hotmail.be>
2025-02-03 10:21:50 +0100

    :sparkles: Add documentation

--- lib/carser.mli
+++ lib/carser.mli
@@ -1,2 +1,5 @@
+(** Module Carser contains submodules {!Parser}, which defines a functor.
+    Other modules are define specific instances of that functor. *)
+
 module Parser : module type of Parser
 module String : module type of String
--- lib/string.mli
+++ lib/string.mli
@@ -1,9 +1,33 @@
+(** Module String contains a parser instance, where the stream type is string and the
+    tokens are chars *)
+
 open Parser
+
+(** A string stream with char tokens, which conforms to the
+    {!Parser.StreamType} signature. *)
 module Stream : StreamType with type t := string and type token := char
+
+(** Includes the main parser functions for convenience.
+
+    @closed *)
 include S with type stream := string and type token := char
 
+(** [char c] returns a parser which succeeds if the next character in the stream
+    is [c]. *)
 val char : char -> char t
+
+(** [any_of characters] returns a parser which succeeds if the next character in
+    the stream is any of [characters]. *)
 val any_of : char list -> char t
+
+(** [string s] returns a parser which succeeds if the next characters in the
+    stream form [s]. *)
 val string : string -> string t
+
+(** [uint] returns a parser which succceeds if the next characters in the stream
+    form an unsigned integer. *)
 val uint : int t
+
+(** [int] returns a parser which succceeds if the next characters in the stream
+    form an signed integer. *)
 val int : int t
